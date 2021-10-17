f3117bb6e033781dbafcd77f145c06d65ee89c51
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-10-17 20:47:18 +0200

    Mappings and view updates

--- src/main/java/com/dawidgorski/MappingNames.java
+++ src/main/java/com/dawidgorski/MappingNames.java
@@ -1,9 +1,15 @@
 package com.dawidgorski;
 
 public final class MappingNames {
-    final static String ADD_FLASHCARD="add";
-    final static String FLASHCARDS="flashcards";
-    final static String REDIRECT_FLASHCARDS="redirect:/flashcards";
-    final static String HOME_MAPPING="home_mapping";
-    final static String GROUP_MAPPING="group_mapping";
+    final static String HOME_MAPPING = "index";
+
+    final static String FLASHCARDS = "flashcards";
+    final static String ADD_FLASHCARD = "add_flashcard";
+    final static String DELETE_FLASHCARD = "delete_flashcard/";
+    final static String REDIRECT_FLASHCARDS = "redirect:/" + FLASHCARDS;
+
+    final static String LESSONS_MAPPING = "lessons";
+    final static String ADD_LESSON = "add_lesson";
+    final static String DELETE_LESSON = "delete_lesson/";
+    final static String REDIRECT_LESSONS = "redirect:/" + LESSONS_MAPPING;
 }
--- src/main/java/com/dawidgorski/ViewNames.java
+++ src/main/java/com/dawidgorski/ViewNames.java
@@ -3,5 +3,5 @@ package com.dawidgorski;
 public final class ViewNames {
     final static String HOME="index";
     final static String FLASHCARDS="flashcards";
-    final static String GROUPS="groups";
+    final static String LESSONS="lessons";
 }
