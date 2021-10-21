69ac113fa93bb42100d37f41db39a85fefda334f
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-10-21 20:40:56 +0200

    Refactoring

--- src/main/java/com/dawidgorski/FlashcardApplication.java
+++ src/main/java/com/dawidgorski/FlashcardApplication.java
@@ -3,13 +3,13 @@ package com.dawidgorski;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FlashcardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlashcardApplication.class, args);
+
     }
 
 }
