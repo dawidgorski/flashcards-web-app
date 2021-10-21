80513eedc6cdf3e07516be1ce53331cedabe5c12
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-10-21 20:35:20 +0200

    Add custom method

--- src/main/java/com/dawidgorski/FlashcardRepository.java
+++ src/main/java/com/dawidgorski/FlashcardRepository.java
@@ -4,6 +4,10 @@ import com.dawidgorski.model.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
-public interface FlashcardRepository extends JpaRepository<Flashcard,Long> {
+public interface FlashcardRepository extends JpaRepository<Flashcard,Long>{
+    List<Flashcard> findAllByLessonId(Long id);
 }
+
+
